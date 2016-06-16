39215bf25a05e0cdc81feeb307041d2de29ff3b8
Tom Bombadil <amanibhavam@destructuring.org>
2016-06-15 23:34:07 -0700

    fogg config for apps

---
+++ fogg.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+}
+
+module "app" {
+  source = "../module-aws-app"
+
+  bucket_remote_state = "${var.bucket_remote_state}"
+  context_org = "${var.context_org}"
+  context_env = "${var.context_env}"
+}
+
+module "default" {
+  source = "../module-aws-service"
+
+  bucket_remote_state = "${var.bucket_remote_state}"
+  context_org = "${var.context_org}"
+  context_env = "${var.context_env}"
+
+  cidr_blocks = "${var.cidr_blocks}"
+
+  az_count = "${var.az_count}"
+}
