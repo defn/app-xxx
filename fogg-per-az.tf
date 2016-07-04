572fc6f3399045ce3ea83411fbbf5dd64b074d62
Tom Bombadil <amanibhavam@destructuring.org>
2016-07-03 20:00:32 -0700

    fogg config for per az

---
+++ fogg-per-az.tf
@@ -0,0 +1,28 @@
+provider "aws" {
+}
+
+module "app" {
+  source = "../module-aws-app"
+
+  bucket_remote_state = "${var.bucket_remote_state}"
+  context_org = "${var.context_org}"
+  context_env = "${var.context_env}"
+
+  app_name = "${var.app_name}"
+}
+
+module "default" {
+  source = "../module-aws-per-az"
+
+  bucket_remote_state = "${var.bucket_remote_state}"
+  context_org = "${var.context_org}"
+  context_env = "${var.context_env}"
+
+  cidr_blocks = "${var.cidr_blocks}"
+
+  az_count = "${var.az_count}"
+
+  app_name = "${var.app_name}"
+  service_name = "default"
+  app_service_name = "${var.app_name}-default"
+}
